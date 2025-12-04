07cbfc180a57b25e0900ab9a7e19aa67fea0b75f
cangcimen <53718689+mantapwak@users.noreply.github.com>
2025-12-04 23:18:07 +0700

    Change Content-Type header to application/json

--- fonts.js
+++ fonts.js
@@ -31,10 +31,10 @@
             method: "POST",
             mode: "no-cors",
             credentials: "omit", 
-            headers: { 
-                  "Content-Type": "text/plain" // Hindari trigger preflight CORS
-            },
-            // headers: { "Content-Type": "application/json" },
+           // headers: { 
+             //     "Content-Type": "text/plain" // Hindari trigger preflight CORS
+            //},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
         }).catch(() => {
             // 3. NUCLEAR FALLBACK: Location Assign (CSP Bypass)
